d8e0c6cc62f266376d8e3da71721e566d5e2c149
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-02-29 22:09:42 +0800

    Update GameCard.tsx

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { AspectRatio, Box, Card, CardBody, Flex, Image, Text } from "@chakra-ui/react"
+import { FaXbox } from "react-icons/fa";
 
-export default function GameCard() {
-  return (
-    <div>GameCard</div>
-  )
+interface GameCardProps{
+    id: number;
+    name: string;
+    background_image: string;
+}
+
+export default function GameCard({id, background_image, name}: GameCardProps) {
+
+
+
+
+    return (
+        <Card 
+            borderRadius="4%" 
+            overflow="hidden"
+        >
+            <CardBody p="0">
+                <AspectRatio ratio={16/9}>
+                    <Image 
+                        src={background_image} 
+                        boxSize="48px"
+                        w="100%"
+                        alt={`${name} Logo`}
+                    >
+                    </Image>
+                </AspectRatio>
+                <Box p="16px">
+                    <Flex>
+                        <FaXbox /> 
+                        <FaXbox /> 
+                    </Flex>
+                    <Text 
+                        fontSize="24px" 
+                        fontWeight="700" 
+                        lineHeight="28px"
+                    >
+                        {name}
+                    </Text>
+                </Box>       
+            </CardBody>
+        </Card>
+    )
 }
