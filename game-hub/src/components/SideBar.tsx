933dd02b23bfe57ccb454b6c0cbd03c7cc428d12
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-02-29 15:56:40 +0800

    Update SideBar.tsx

--- game-hub/src/components/SideBar.tsx
+++ game-hub/src/components/SideBar.tsx
@@ -1,11 +1,51 @@
-import { Flex, Heading, VStack } from "@chakra-ui/react";
-
+import { Flex, Heading, List, ListIcon, ListItem, VStack, Link, Image, Text } from "@chakra-ui/react";
 
 export default function SideBar() {
-  return (
-    <VStack alignItems="start">
-        <Heading size="lg" as="h2" textAlign="left">Genres</Heading>
-
-    </VStack>
-  )
+    return (
+        <VStack alignItems="start">
+            <Heading size="lg" as="h2" textAlign="left">Genres</Heading>
+            <List spacing={3}>
+                <ListItem>
+                    <Link href="" display="flex">
+                        <Image 
+                            src="https://cdn.discordapp.com/attachments/907169630538326076/1212659189327986738/genre_dummy_image.png?ex=65f2a3e0&is=65e02ee0&hm=1d33446753e27437896da6acd04de6c6a9924dbb417472d1d2c47d10d0bdbeeb&"
+                            boxSize="32px"
+                            alt="Application Logo">
+                        </Image>
+                        <Text ml="2">Action</Text>
+                    </Link>
+                </ListItem>
+                <ListItem>
+                    <Link href="" display="flex">
+                        <Image 
+                            src="https://cdn.discordapp.com/attachments/907169630538326076/1212659189327986738/genre_dummy_image.png?ex=65f2a3e0&is=65e02ee0&hm=1d33446753e27437896da6acd04de6c6a9924dbb417472d1d2c47d10d0bdbeeb&"
+                            boxSize="32px"
+                            alt="Application Logo">
+                        </Image>
+                        <Text ml="2">Action</Text>
+                    </Link>
+                </ListItem>
+                <ListItem>
+                <Link href="" display="flex">
+                        <Image 
+                            src="https://cdn.discordapp.com/attachments/907169630538326076/1212659189327986738/genre_dummy_image.png?ex=65f2a3e0&is=65e02ee0&hm=1d33446753e27437896da6acd04de6c6a9924dbb417472d1d2c47d10d0bdbeeb&"
+                            boxSize="32px"
+                            alt="Application Logo">
+                        </Image>
+                        <Text ml="2">Action</Text>
+                    </Link>
+                </ListItem>
+                <ListItem>
+                <Link href="" display="flex">
+                        <Image 
+                            src="https://cdn.discordapp.com/attachments/907169630538326076/1212659189327986738/genre_dummy_image.png?ex=65f2a3e0&is=65e02ee0&hm=1d33446753e27437896da6acd04de6c6a9924dbb417472d1d2c47d10d0bdbeeb&"
+                            boxSize="32px"
+                            alt="Application Logo">
+                        </Image>
+                        <Text ml="2">Action</Text>
+                    </Link>
+                </ListItem>
+            </List>
+        </VStack>
+    )
 }
