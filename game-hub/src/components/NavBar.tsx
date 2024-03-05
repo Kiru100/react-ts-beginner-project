47106d61fbad3e39315751f52cfb4eaf6c012a21
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-05 15:29:23 +0800

    Update NavBar.tsx

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -11,9 +11,37 @@ import {
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import Logo from "../assets/logo.webp";
+import React, { useState } from "react";
 
 export default function NavBar() {
     const { colorMode, toggleColorMode } = useColorMode();
+    const [input_value, setInputValue] = useState<string>('');
+
+    // const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>) =>{
+
+    //     if (event.key === 'Enter') {
+    //         console.log('do validate')
+    //     }
+
+    //     setTimeout(()=>{
+    //         console.log("hasdhasdh")
+    //     },200)
+    // }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value)
+
+        setTimeout(()=>{
+            console.log("event", event.target.value)
+        }, 1000);
+    };
+
+    const handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            
+        }
+    };
+ 
 
     return (
             <Flex alignItems="center" w="100%">
@@ -22,15 +50,40 @@ export default function NavBar() {
                     boxSize="48px"
                     alt="Application Logo">
                 </Image>
-                <InputGroup borderRadius="lg" flexGrow="1" ml="4">
+                <InputGroup 
+                    borderRadius="lg" 
+                    flexGrow="1" 
+                    ml="4"
+                >
                     <InputLeftElement pointerEvents="none" >
                         <SearchIcon />
                     </InputLeftElement>
-                    <Input type="search" placeholder="Search Games"  borderRadius="full"/>
+                    <Input 
+                        type="search" 
+                        placeholder="Search Games" 
+                        borderRadius="full"  
+                        value={input_value}
+                        onChange={handleInputChange}
+                        onKeyDown={handleInputKeyDown}
+                    />
                 </InputGroup>
-                <FormControl display="flex" alignItems="center" w="fit-content" ml="6">
-                    <Switch id="color_mode" onChange={toggleColorMode} isChecked={colorMode === "dark" ? true : false}/>
-                    <FormLabel htmlFor="color_mode" mb="0" w="max-content" ml="2">
+                <FormControl 
+                    display="flex" 
+                    alignItems="center" 
+                    w="fit-content" 
+                    ml="6"
+                >
+                    <Switch 
+                        id="color_mode" 
+                        onChange={toggleColorMode} 
+                        isChecked={colorMode === "dark" ? true : false}
+                    />
+                    <FormLabel 
+                        htmlFor="color_mode" 
+                        mb="0" 
+                        w="max-content" 
+                        ml="2"
+                    >
                         {colorMode === "light" ? "Light Mode" : "Dark Mode" }
                     </FormLabel>
                 </FormControl>
