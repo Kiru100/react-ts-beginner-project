8e323ee57ae8253227d22d64d13eab76fa5ba6f4
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-06 11:09:20 +0800

    Update Main.tsx

--- game-hub/src/components/Main.tsx
+++ game-hub/src/components/Main.tsx
@@ -1,11 +1,34 @@
 import { Container, Heading } from "@chakra-ui/react"
 import GameList from "./GameList"
 
-export default function Main() {
+
+interface Game {
+    id: number;
+    name: string;
+    added: number;
+    background_image: string;
+    metacritic: number;
+    parent_platforms: {
+        platform: {
+            id: number;
+            name: string;
+            slug: string;
+        };
+    }[];
+}
+
+interface GameProps{
+    games_data: Game[]
+    genre_selected: string;
+}
+
+export default function Main({games_data, genre_selected}: GameProps) {
     return (
             <Container centerContent={false} alignSelf="left" w="100%" maxW="none">
-                <Heading p="2" h="56px">Games</Heading>
-                <GameList search_value="asdasd"></GameList>
+                <Heading p="2" h="56px">
+                    {genre_selected} Games               
+                </Heading>
+                <GameList games_data={games_data}></GameList>
 
                 
             </Container>
