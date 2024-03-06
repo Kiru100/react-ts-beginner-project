5b5792c4ee4c1ec5e47c03e5bfb1c7b365f54a22
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-06 18:55:23 +0800

    Update App.tsx

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, GridItem, useColorMode } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show, useColorMode } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
 import Main from "./components/Main";
@@ -100,9 +100,13 @@ function App() {
 
     return (
             <Grid
-                templateAreas={`"header header"
-                                "nav main"
-                                "nav footer"`}
+                // templateAreas={`"header header"
+                //                 "nav main"
+                //                 "nav footer"`}
+                templateAreas={{
+                    base: `"header header" "main main"`,
+                    lg:  `"header header" "nav main" "nav footer"`
+                }}
                 gridTemplateRows={"50px 1fr"}
                 gridTemplateColumns={"200px 1fr"}
                 gridRowGap="8"
@@ -111,9 +115,11 @@ function App() {
                 <GridItem pt="2" area={"header"}>
                     <NavBar handleSearch={handleSearch}/>
                 </GridItem>
-                <GridItem pl="2" area={"nav"}>
-                    <SideBar handleGenreClick={handleGenreClick}/>
-                </GridItem>
+                <Show above="lg">
+                    <GridItem pl="2" area={"nav"}>
+                        <SideBar handleGenreClick={handleGenreClick}/>
+                    </GridItem>
+                </Show>
                 <GridItem pl="2" area={"main"}>
                     <Main 
                         games_data={games} 
