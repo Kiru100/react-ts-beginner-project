97c646f7e935ed619e1ac57ac2ace49001f81fb6
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-09 21:06:43 +0800

    Create interfaces.ts

---
+++ game-hub/src/services/interfaces.ts
@@ -0,0 +1,22 @@
+
+
+export interface Game {
+    id: number;
+    name: string;
+    added: number;
+    background_image: string;
+    metacritic: number;
+    parent_platforms: {
+        platform: {
+            id: number;
+            name: string;
+            slug: string;
+        };
+    }[];
+}
+
+export interface Platform{
+    id: number;
+    name: string;
+    slug: string;
+}
